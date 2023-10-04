24c3547cd5050ab078af908755686b63b8dab1e8
StudentA415 <StudentA415@institut-it.mirea.ru>
2023-10-04 19:01:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import Some.Body;
+
+import java.util.Scanner;
+
+v2.1.1.1
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+//        int b = scanner.nextInt(); // ctrl + / -- быстрое комментирование
+//        System.out.println(a + b);
+
+
+
+
+//        String s = scanner.nextLine(); // сканиирует строку
+//        System.out.println(s); // / -- деление, подстраивается под тип числа
+
+
+
+        byte b = 10 >> 1; // побитовое сдвижение (згать не надо)
+
+        try {
+            float a = scanner.nextFloat();
+            System.out.println(  Math.pow((int) a , 2));
+        } catch (Exception e){ //Exeption -- ловит ошибку   e -- имя ошибки
+            System.out.println("Ацтань");
+            System.out.println(e);
+        }finally { //выполняется всегда
+            System.out.println("\n Я выполнюсь всегда!");
+        }
+
+
+
+
+
+
+
+
+    }
+}
